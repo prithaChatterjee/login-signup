f827cedec360ad3f53b8f81140f9e999df0deedb
prithaChatterjee <pchatterjee518@gmail.com>
2021-10-30 22:23:44 +0530

    updates for today

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -19,22 +19,23 @@ const formSubmit = (values: Object) => {
 }
 
 const Login = () => {
-    const { formValues, handleInputChange, errors, onSubmit} = useForm<formValuesProps, errorProps>({
+    const { formValues, handleInputChange, errors, onSubmit} = useForm<formValuesProps>({
         email: "",
         password: ""
     }, {
         email_error: "",
         password_error: ""
-    } as errorProps)
+    })
     return (
         <form noValidate className={loginPageStyle.loginBg} onSubmit={(e) => onSubmit(e, formSubmit)}>
             <Card className={`${loginPageStyle.m_auto} ${loginPageStyle.cardBg}`}>
                 <Input style={{ marginBottom: "1.5rem" }} value={formValues.email} type="text" name='email' required
                     label="Enter Your Email here"
-                    error={errors.email_error} helpertext={errors.email_error} onChange={(e) => handleInputChange(e)} />
+                    error={errors.email_error ? true : false} helpertext={errors.email_error} 
+                    onChange={(e) => handleInputChange(e,true, e.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? true : false)} />
                 <Input style={{ marginBottom: "1.5rem" }} value={formValues.password} type="password" name='password' label="Password"
-                    required error={errors.password_error} helpertext={errors.password_error}
-                    onChange={(e) => handleInputChange(e)} />
+                    required error={errors.password_error? true: false} helpertext={errors.password_error}
+                    onChange={(e) => handleInputChange(e, true, e.target.value.length >= 8)} />
                 <div className={loginPageStyle.buttonBg}>
                     <Button type="submit">Submit</Button>
                 </div>
--- src/hook/useForm.ts
+++ src/hook/useForm.ts
@@ -1,41 +1,42 @@
 import { ChangeEvent, FormEvent, useState } from "react"
 
 type InitailValuesTypes<IT> = IT
-type errorsValuesTypes<ET> = ET
-export default function useForm<IT extends {}, ET extends {}> (initailValues: InitailValuesTypes<IT>, 
-    errorsValues: errorsValuesTypes<ET>) {
-    const [formValues, setformValues] = useState<any>(initailValues)
-    const [errors, seterrors] = useState<any>(errorsValues)
+type errorsValuesTypes = { [key: string]: string }
+export default function useForm<IT extends { [key: string]: string | number | null }>(initailValues: InitailValuesTypes<IT>,
+    errorsValues: errorsValuesTypes) {
+    const [formValues, setformValues] = useState<IT>(initailValues)
+    const [errors, seterrors] = useState<errorsValuesTypes>(errorsValues)
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>, validtype = true , validationType = true) => {
-        setformValues({...formValues, [e.target.name]: e.target.value ? validtype ? e.target.value : 
-        formValues[e.target.name] : ""})
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>, validtype = true, validationType = true) => {
+        setformValues({
+            ...formValues, [e.target.name]: e.target.value ? validtype ? e.target.value :
+                formValues[e.target.name] : ""
+        })
         if (e.target.required) {
             if (!e.target.value) {
-                seterrors({...errors, [`${e.target.name}_error`]: `${e.target.name} is required`})
-            }else{
+                seterrors({ ...errors, [`${e.target.name}_error`]: `${e.target.name} is required` })
+            } else {
                 delete errors[`${e.target.name}_error`]
-                seterrors({...errors})
+                seterrors({ ...errors })
             }
+        }
+        if (!validationType) {
+            seterrors({ ...errors, [`${e.target.name}_error`]: `${e.target.name.replace(/_/g, " ")} is not Valid` })
         } else {
-            if (!validationType) {
-                seterrors({...errors, [`${e.target.name}_error`]: `${e.target.name.replace(/_/g, " ")} is not Valid`})
-            }else{
-                delete errors[`${e.target.name}_error`]
-                seterrors({...errors})
-            }
+            delete errors[`${e.target.name}_error`]
+            seterrors({ ...errors })
         }
     }
 
-    const onSubmit = (e: FormEvent, submit : (formValue: IT) => void) => {
+    const onSubmit = (e: FormEvent, submit: (formValue: IT) => void) => {
         e.preventDefault()
         if (Object.entries(errors).length === 0) {
             submit(formValues)
         } else {
             Object.keys(errors).map(res => errors[res] = `${(res.substring(0, res.length - 5)).replace(/_/g, " ")} is required`)
-            seterrors({...errors})
+            seterrors({ ...errors })
         }
     }
 
-    return {formValues, handleInputChange, errors, onSubmit}
+    return { formValues, handleInputChange, errors, onSubmit }
 }
